bd689e3295615579441b60786131b28df32e4ab8
erlind <erlind@erlinds-MacBook-Pro.local>
2023-06-08 12:46:39 +0200

    small fixes

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,8 @@
-import express from 'express';
-import * as bodyParser from 'body-parser';
-import cors from 'cors';
-import helmet from 'helmet';
-import { getImages } from './features/getImages/getImages';
+import express from "express";
+import * as bodyParser from "body-parser";
+import cors from "cors";
+import helmet from "helmet";
+import { getImages } from "./features/getImages/getImages";
 
 const app = express();
 
@@ -10,10 +10,12 @@ app.use(helmet());
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-app.disable('x-powered-by');
+app.disable("x-powered-by");
 
-app.post('/gallery', getImages);
+app.post("/gallery", getImages);
 
-app.listen(9000, () => {
-    console.log('server listening on port :9000');
-})
+const port = process.env.PORT || 9000;
+
+app.listen(port, () => {
+  console.log("server listening on port :9000");
+});
